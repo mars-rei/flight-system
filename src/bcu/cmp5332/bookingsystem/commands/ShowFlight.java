fcff00e607a40600a48efabe7695d676753639fa
imiel <imiel@kasei>
2024-12-15 17:08:57 +0000

    testing if commands are working - working but need to be fully implemented - mars

--- src/bcu/cmp5332/bookingsystem/commands/ShowFlight.java
+++ src/bcu/cmp5332/bookingsystem/commands/ShowFlight.java
@@ -31,6 +31,6 @@ public class ShowFlight implements Command {
     public void execute(FlightBookingSystem flightBookingSystem) throws FlightBookingSystemException {
         // TODO: implementation here
     	Flight flight = flightBookingSystem.getFlightByID(id);
-    	flight.getDetailsLong(); // need to implement this method in the model customer class still
+    	System.out.println(flight.getDetailsLong()); // need to implement this method in the model customer class still
     }
 }
